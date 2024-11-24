e989e655782e07ad581a7921c4c24bb857d1658d
privelosos <priscilavelosoconsultoria@gmail.com>
2024-11-24 11:23:20 -0400

    inserido JS search >> localização

---
+++ localizacao/localizacao.js
@@ -0,0 +1,21 @@
+const inputCEP = document.querySelector('.search input');
+const buttonEncontrar = document.querySelector('.search button');
+const divResultados = document.createElement('div');
+document.querySelector('.search').appendChild(divResultados);
+
+buttonEncontrar.addEventListener('click', () => {
+  const cep = inputCEP.value;
+  const bairro = getBairroFromCEP(cep);
+
+  if (bairro === 'Centro') {
+    divResultados.innerHTML = `
+      <p>Loja Centro: Av. Afonso Pena, 2024.</p>
+    `;
+  } else if (bairro === 'Jardim dos Estados') {
+    divResultados.innerHTML = `
+      <p>Loja Jardim dos Estados: Rua Euclides da Cunha, 3600.</p>
+    `;
+  } else {
+    divResultados.innerHTML = '<p>Não atendemos sua região.</p>';
+  }
+});
